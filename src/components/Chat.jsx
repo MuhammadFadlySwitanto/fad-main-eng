dc5d4d2bb6d1e68ed74186f54dd1f16c4e8ed7d1
farizrivaldo <khaerul.fariz98@gmail.com>
2025-06-16 06:04:13 +0000

    Commit sementara sebelum pull

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -200,10 +200,6 @@ const Chat = () => {
     }
   };
 
-
- 
-
-
   return (
   <>
     <button className="fixed bottom-4 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 border rounded-full w-16 h-16 bg-black hover:bg-gray-700 m-0 cursor-pointer border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900" type="button" aria-haspopup="dialog" aria-expanded={isOpen ? "true" : "false"} onClick={toggleChatbot}>
